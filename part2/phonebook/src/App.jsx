f05073589e22307381213821c2bc68fb88d47078
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-05-25 01:48:26 +0300

    finish exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setNameFilter] = useState([]);
-
+  useEffect(() => {
+    axios.get(" http://localhost:3001/persons").then((res)=>{
+    console.log(res.data)
+    setPersons(res.data)
+    })
+  },[]);
   const handleOnChangeName = (e) => {
     console.log(e.target.value);
     setNewName(e.target.value);
@@ -62,7 +63,7 @@ const App = () => {
         handleOnSubmit={handleOnSubmit}
       />
       <h2>Numbers</h2>
-     <Persons persons={persons} />
+      <Persons persons={persons} />
     </div>
   );
 };
