dfa7e0e538aadc9f52b75d78b5cfe01b5520799f
stbs0 <mohammedjrt@gmail.com>
2024-06-02 13:31:31 +0300

    finish exercise 4.9

--- part4/app.js
+++ part4/app.js
@@ -1,5 +1,6 @@
 const config = require("./utilities/config");
 const express = require("express");
+require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,17 +1,48 @@
-const blogsRouters = require("express").Router()
-const Blog = require("../models/blog")
-blogsRouters.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+const blogsRouters = require("express").Router();
+const { request, response } = require("../app");
+const Blog = require("../models/blog");
+blogsRouters.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouters.post("/", (request, response) => {
-  console.log(request.body);
+blogsRouters.post("/", async (request, response) => {
+  if (request.body.title === undefined) {
+    response.status(400).end({ error: "title is not missing" });
+  }
+  if (request.body.url === undefined) {
+    response.status(400).end({ error: "url is not missing" });
+  }
   const blog = new Blog(request.body);
-  console.log(blog);
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+
+  blog.likes = blog.likes || 0;
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
-module.exports = blogsRouters
+
+blogsRouters.delete("/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+blogsRouters.get("/:id", async (request, response) => {
+  const blog = (await Blog.find({})).map(
+    (blog) => (blog._id = request.params.id)
+  );
+  response.json(blog);
+});
+
+blogsRouters.put("/:id", async (req, res) => {
+  const body = req.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+  const updated = await Blog.findByIdAndUpdate(req.params.id, blog,{new:true});
+
+  res.json(updated)
+});
+module.exports = blogsRouters;
