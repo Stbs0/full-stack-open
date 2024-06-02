e38d582be5cbe2460162a33429d7b770212fde94
stbs0 <mohammedjrt@gmail.com>
2024-06-02 13:31:31 +0300

    finish exercise 4.9

--- part4/tests/blogs.test.js
+++ part4/tests/blogs.test.js
@@ -1,67 +1,152 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-const api = supertest(app)
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
+const api = supertest(app);
+const Blog = require("../models/blog");
+const assert = require("node:assert");
+const helper = require("./test.helper");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+test("return all blogs", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("verify blogs id", async () => {
+  const res = await helper.BlogsInDb();
+
+  const isIdPresent = res.every((blog) => Object.keys(blog).includes("id"));
+
+  assert.strictEqual(isIdPresent, true);
+});
+
+test("verify creating a new blog", async () => {
+  const newBlog = {
+    title: "dhrt",
+    author: "ukyuf",
+    url: "http://localhost:3003/api/blogs",
+    likes: 47435,
+  };
+  const beforeRes = await helper.BlogsInDb();
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const afterRes = await helper.BlogsInDb();
+  assert.strictEqual(afterRes.length, beforeRes.length + 1);
+});
+
+test("verify saved blog", async () => {
+  const newBlog = {
+    title: "dthzth",
+    author: "zdthdtn",
+    url: "http://blog.cleancothdzder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 67,
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const allBlogs = await helper.BlogsInDb();
+  const savedBlog = allBlogs.filter((blog) => {
+    return (
+      blog.title === newBlog.title &&
+      blog.author === newBlog.author &&
+      blog.url === newBlog.url &&
+      blog.likes === newBlog.likes
+    );
+  });
+
+  assert.strictEqual(savedBlog[0].title, newBlog.title);
+  assert.strictEqual(savedBlog[0].author, newBlog.author);
+  assert.strictEqual(savedBlog[0].url, newBlog.url);
+  assert.strictEqual(savedBlog[0].likes, newBlog.likes);
+});
+
+test("verify default like blog", async () => {
+  const newBlog = {
+    title: "Rhvhjvhv",
+    author: "Milbljb",
+    url: "https://reactpatterns.com/",
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const allBlogs = await helper.BlogsInDb();
+  const savedBlog = allBlogs.filter((blog) => {
+    return (
+      blog.title === newBlog.title &&
+      blog.author === newBlog.author &&
+      blog.url === newBlog.url
+    );
+  });
+  assert.strictEqual(savedBlog[0].likes, 0);
+});
+
+test("verify title is missing blog", async () => {
+  const newBlog = {
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 2,
+  };
+
+  await api.post("/api/blogs").send(newBlog).expect(400);
+});
+test("verify url have blog", async () => {
+  const newBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+
+    likes: 2,
+  };
+  await api.post("/api/blogs").send(newBlog).expect(400);
+});
+
+test("delete certain blog", async () => {
+  const allBlogs = await helper.BlogsInDb();
+  const deleteOne = allBlogs[0];
+  await api.delete(`/api/blogs/${deleteOne.id}`).expect(204);
+  const res = await helper.BlogsInDb();
+
+  assert.strictEqual(res.length, helper.initialBlogs.length - 1);
+});
+
+test("test updating the likes of the blog", async () => {
+  const allBlogs = await helper.BlogsInDb();
+  const old = allBlogs[0];
+  const newBlog= {
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0,
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0,
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0,
-  },
-];
+    likes: 20,
+  };
 
-test("return all blogs",()=>{
-    api
-      .get("/api/blogs")
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
+ const updated= await api
+    .put(`/api/blogs/${old.id}`)
+    .send(newBlog)
+    .expect("Content-Type", /application\/json/);
 
+const end = await helper.BlogsInDb();
 
-})
-after( () => {
-   mongoose.connection.close();
+assert.strictEqual(end.length, allBlogs.length)
+
+assert.strictEqual(end[0].likes, 20)
+});
+
+after(async () => {
+  await mongoose.connection.close();
 });
