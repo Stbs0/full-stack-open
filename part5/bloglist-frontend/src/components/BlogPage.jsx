5cc406c2959a7c62768d0e9887f4da0b7cba9c01
stbs0 <mohammedjrt@gmail.com>
2024-06-05 04:29:07 +0300

    change laptop --- problem

--- part5/bloglist-frontend/src/components/BlogPage.jsx
+++ part5/bloglist-frontend/src/components/BlogPage.jsx
@@ -1,44 +1,39 @@
-import { forwardRef,useState,useRef } from "react";
+import { forwardRef, useState } from "react";
 import Blog from "./Blog";
 import BlogForm from "./BlogForm";
 import Notification from "./Notification";
 import Togglable from "./Togglable";
 
-const BlogPage = forwardRef(
-  (props,refs) => {
-     const [newBlog, setNewBlog] = useState({
-    title: "",
-    author: "",
-    url: "",
-  });
-  const blogFormRef = useRef()
-    return (
+const BlogPage = forwardRef((props, refs) => {
+  return (
+    <div>
+      <h1>blogs</h1>
+      {props.message || props.errorMessage ? (
+        <Notification
+          message={props.message}
+          errorMessage={props.errorMessage}
+        />
+      ) : null}
+      <p>
+        {props.user.name} logged in{" "}
+        <button onClick={props.handleLogout}>logout</button>
+      </p>
+      <Togglable
+        buttonLabel='create Blog'
+        ref={refs}>
+        <BlogForm createBlog={props.handleCreateBlog} />
+      </Togglable>
+      // TODO fix this. it dont re render on inserting the blog
       <div>
-        <h1>blogs</h1>
-        {props.message || props.errorMessage ? (
-          <Notification
-            message={props.message}
-            errorMessage={props.errorMessage}
-          />
-        ) : null}
-        <p>
-          {props.user.name} logged in{" "}
-          <button onClick={props.handleLogout}>logout</button>
-        </p>
-        <Togglable buttonLabel="create Blog" ref={refs}>
-          <BlogForm
-            ref={blogFormRef}
-            newBlog={props.newBlog}
-            handleCreateBlog={props.handleCreateBlog}
-            setNewBlog={props.setNewBlog}
-          />
-        </Togglable>
-
-        <div>
-          <Blog userBlogs={props.userBlogs} />
-        </div>
+        {props.userBlogs.map((blog) => {
+          <Blog
+            key={blog.id}
+            blog={blog}
+          />;
+          console.log(blog);
+        })}
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
 export default BlogPage;
