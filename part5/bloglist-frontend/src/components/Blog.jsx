441cb1fee745f8c90183b5b6ffd6506540e84613
stbs0 <mohammedjrt@gmail.com>
2024-06-06 01:56:59 +0300

    finished last exercise in part 5 b

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-const Blog = ({ blog, handleUpdateBlog }) => {
+const Blog = ({ blog, handleUpdateBlog, user, handleDeleteBlog }) => {
   const [showDetails, setShowDetails] = useState(false);
-  console.log("blog");
 
   const blogStyle = {
     paddingTop: 10,
@@ -14,7 +13,6 @@ const Blog = ({ blog, handleUpdateBlog }) => {
   const handleClick = () => {
     setShowDetails(!showDetails);
   };
-  console.log("blog",blog);
   return (
     <div style={blogStyle}>
       <p>
@@ -25,18 +23,22 @@ const Blog = ({ blog, handleUpdateBlog }) => {
       <div style={showDetails ? { display: "block" } : { display: "none" }}>
         <p>{blog.url}</p>
         <p>
-          {blog.likes}{" "}
+          {blog.likes}
           <button
-            onClick={() =>
+            onClick={(e) => {
               handleUpdateBlog(
+                e,
                 { ...blog, likes: String(+blog.likes + 1) },
                 blog.id,
-              )
-            }>
+              );
+            }}>
             likes
           </button>
         </p>
         <p>{blog.author}</p>
+        {user.username === blog.user.username ? (
+          <button onClick={()=>handleDeleteBlog(blog.id,blog.title)}>remove</button>
+        ) : null}
       </div>
     </div>
   );
