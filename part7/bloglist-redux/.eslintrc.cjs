4426163584978a6643d1da092b7d29711bce1a18
stbs0 <mohammedjrt@gmail.com>
2024-06-14 03:17:22 +0300

    finish  setting up  exercise 7.8

--- part7/bloglist-redux/.eslintrc.cjs
+++ part7/bloglist-redux/.eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
@@ -17,8 +18,6 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
