dad314bbfb7108740176978ce9af07ddc0735782
stbs0 <mohammedjrt@gmail.com>
2024-06-15 06:23:41 +0300

    changing laptop

--- part7/bloglist-query/.eslintrc.cjs
+++ part7/bloglist-query/.eslintrc.cjs
@@ -9,16 +9,15 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
--- part7/bloglist-query/src/services/blogs.js
+++ part7/bloglist-query/src/services/blogs.js
@@ -1,13 +1,17 @@
 import axios from "axios";
-const baseUrl = "/api/blogs";
+const baseUrl = "http://localhost:3001/api/blogs";
 let token = null;
 
 const setToken = (newToken) => {
   token = `Bearer ${newToken}`;
 };
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((res) => res.data);
+const getAll = async () => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const request = axios.get(baseUrl, config);
+  const res = await request;
+  return res.data;
 };
 const create = async (newObject) => {
   const config = {
