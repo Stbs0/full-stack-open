6feb78490f01a25ec091ac202737ab35cc60da1d
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-06-19 03:17:15 +0300

    during part 2 exercise 7.15 View

--- part7/views/src/App.jsx
+++ part7/views/src/App.jsx
@@ -1,37 +1,68 @@
 import BlogPage from "./components/BlogPage";
 import LogInForm from "./components/LoginPage";
 import Users from "./components/Users";
-import { useUserValue } from "./UserContext"; import { useQuery } from "@tanstack/react-query";
-import blogService from "./services/blogs"; 
-import User from "./components/User"
-import { BrowserRouter as Router, Routes, Route, Link,Navigate  } from "react-router-dom";
+import { useUserValue } from "./UserContext";
+import { useQuery } from "@tanstack/react-query";
+import blogService from "./services/blogs";
+import User from "./components/User";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNotificationDispatch } from "./NotificationContext";
+import { useUserDispatcher } from "./UserContext";
 // import { createSuccessMsg, createErrorMsg } from "./actions";
-// import { userLogOut, userLogIn } from "./actions";
+import { userLogIn } from "./actions";
 
 // FIXME: users arnt saved so the login page appear instead of users page no matter what
 /* TODO // 
 create a use to save the logged in of the user refer to execise 7.13?
 */
 const App = () => {
+  const userDispatcher = useUserDispatcher();
+  
+
   const user = useUserValue();
   console.log(user);
-  const usersData = useQuery({
-    queryKey: ["blogs"],
-    queryFn: blogService.getAllUsers,
-    refetchOnWindowFocus: false,
-  });
+  // const usersData = useQuery({
+  //   queryKey: ["blogs"],
+  //   queryFn: blogService.getAllUsers,
+  //   refetchOnWindowFocus: false,
+  // });
   return (
     <Router>
       <Routes>
-      {/* added props to the User component */}
-      <Route path="/users/:id" element={<User users={usersData.data}/>} />
+        {/* added props to the User component */}
+        <Route
+          path='/users/:id'
+          element={<User users={user} />}
+        />
         <Route
           path='/users'
-          element={user.username === "" ? <Navigate replace to={"/"}/> : <Users />}
+          element={   user ? (
+              <Navigate
+                replace
+                to={"/"}
+              />
+            ) : (
+               <Users />)}
         />
         <Route
           path='/'
-          element={<LogInForm />}
+          element={
+            user ? (
+              <Navigate
+                replace
+                to={"/users"}
+              />
+            ) : (
+              <LogInForm />
+            )
+          }
         />
       </Routes>
     </Router>
